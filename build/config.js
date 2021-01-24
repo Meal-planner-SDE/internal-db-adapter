99f7b88ca0a958e018419ee5965bff0f3d0f0132
Gabriele Masina <gabriele.masina@virgilio.it>
2021-01-24 21:09:17 +0100

    Remove build

--- build/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    PORT: 80,
-    HOST: '0.0.0.0',
-    URL_API_DATA: 'http://172.16.238.10',
-};
